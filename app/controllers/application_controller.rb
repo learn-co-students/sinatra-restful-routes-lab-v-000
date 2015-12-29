935e378cfd29b99a745cb607eecce95bf30af061
plai217 <philiplai217@gmail.com>
2015-12-29 17:27:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    @recipe.save
     redirect to "/recipes/#{@recipe.id}"
   end
 
