01e9633559928e29657068a9f9e5cbb0045ad1c7
Karuna <imkaruna@gmail.com>
2016-01-29 19:35:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,53 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.new
+    @recipe.name = params["name"]
+    @recipe.ingredients = params["ingredients"]
+    @recipe.cook_time = params["cook_time"]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes/:id/delete' do #delete action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.destroy
+  redirect '/recipes'
+  end
+
+  get '/recipes/:id/edit' do #edit the recipe with the given :id
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/recipes/:id' do #update and save the edited recipe
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:recipe_name], ingredients: params[:ingredients_names], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
 end
