015bf43d06a35519a61df167f70fdc05402cf974
coltonstaab1 <colton.staab@gmail.com>
2016-02-03 01:08:20 +0000

    Done.

---
+++ db/migrate/01_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration
+
+	def change
+		create_table :recipes do |t|
+			t.string :name
+			t.string :ingredients
+			t.decimal :cook_time		
+		end
+	end
+
+end
