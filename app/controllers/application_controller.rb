dca08b94cb65424a6df8b4a210448b820ee3d9b6
Dakota Martinez <dakotaleemusic@gmail.com>
2016-02-15 07:01:21 +0000

    adds index, new, and show features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,27 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
   get '/' do
+    @recipes = Recipe.all
     erb :index
   end
+  
+  get "/recipes" do 
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get "/recipes/new" do 
+    erb :new
+  end
+  
+  post "/recipes" do 
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+  
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+  
 end
