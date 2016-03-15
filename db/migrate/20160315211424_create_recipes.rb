3345d9801a3f349da1b94f6f4316ce3be5b22ef9
thelucasmoore <lucasedwardmoore@gmail.com>
2016-03-15 18:04:09 -0500

    Done.

---
+++ db/migrate/20160315211424_create_recipes.rb
@@ -0,0 +1,9 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+  	create_table :recipes do |r|
+  		r.string :name
+  		r.string :ingredients
+  		r.string :cook_time
+  	end
+  end
+end
