eb1571c6a9732a15f6d5a8f665ea5c55cf4d2ee9
jmheyd <heydjm@comcast.net>
2016-04-13 13:53:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,57 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+
   get '/' do
-    erb :index
+  	redirect to '/recipes'
   end
-end
+
+#index page to display all recipes
+  get '/recipes' do
+  	@recipes = Recipe.all 
+  	erb :index
+  end
+  
+#render form for new recipe
+  get '/recipes/new' do  
+  	erb :new
+  end
+
+#create and save  new recipe
+  post '/recipes' do
+  	@recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], 
+  		:cook_time => params[:cook_time])
+  	redirect "/recipes/#{@recipe.id}"
+  end
+
+#show one single recipe based on id
+  get '/recipes/:id' do 
+  	@recipe = Recipe.find_by_id(params[:id])
+  	erb :show 
+  end
+
+#get edit form
+  get '/recipes/:id/edit' do  
+  	@recipe = Recipe.find_by_id(params[:id])
+  	erb :edit
+  end
+
+#edit one recipe
+  post '/recipes/:id' do
+  	@recipe = Recipe.find_by_id(params[:id])
+  	@recipe.name = params[:name]
+  	@recipe.ingredients = params[:ingredients]
+  	@recipe.cook_time = params[:cook_time]
+  	@recipe.save
+  	redirect to "/recipes/#{@recipe.id}"
+  end
+
+#delete one recipe
+  post '/recipes/:id/delete' do
+  	@recipe = Recipe.find_by_id(params[:id])
+  	@recipe.delete
+  	redirect to '/recipes'
+  end
+
+ end
