3c2ba2109f3c7fbc0bfd4cb21394d08f2e4959c0
McN33ly <rmcneely23@gmail.com>
2016-04-25 23:56:07 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes/new' do
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    puts "Sandwhich"
+    @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    binding.pry
+    @recipe.save
     redirect to '/recipes/#{@recipe.id}'
   end
 
