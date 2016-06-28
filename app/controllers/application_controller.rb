2ca5a88ef1b87a8e3c51339053f2dec33218b44c
Christian Whitesides <whitesidescr@gmail.com>
2016-06-28 13:42:40 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,13 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    redirect to '/recipes'
+  get '/recipes/new' do
+    erb :new
   end
 
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
-  end
-
-  get '/recipes/new' do
-    erb :new
-  end
-
-  post '/recipes' do
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
@@ -38,6 +29,12 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    # @recipe = Recipe.create(params)
     redirect to "/recipes/#{@recipe.id}"
   end
 
