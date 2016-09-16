6ae0e8a41aad4951abd2a4e5a1e69f7f2afdbdd8
Jason Casares <jasonc@csdrafting.com>
2016-09-16 20:03:50 +0000

    Done.

--- db/migrate/20160721202021_create_recipes.rb
+++ db/migrate/20160721202021_create_recipes.rb
@@ -1,4 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
   end
 end
