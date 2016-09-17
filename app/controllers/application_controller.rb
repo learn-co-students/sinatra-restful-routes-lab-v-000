15de4f7de0659f0c9c70c889cef0cfb385e98d4e
nicolefederici <nicolefederici@yahoo.com>
2016-09-16 20:55:52 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,44 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/new' do
+  erb :new
+  end
+
+  get '/recipes/:id' do
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :show
+  end
+
+  get '/recipes/:id/edit' do  #load edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do #edit action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.title = params[:title]
+  @recipe.content = params[:content]
+  @recipe.save
+  redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do
+  @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do #delete action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe_name = @recipe.name
+  @recipe.delete
+  redirect to '/recipes'
+end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
