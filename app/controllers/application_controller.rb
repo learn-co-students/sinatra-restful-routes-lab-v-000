c76db76f27d3c5bf3b8dcff6336dfea075511125
auranbuckles <auran59@gmail.com>
2016-09-19 17:08:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
+require 'pry'
+require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
+  end
+
+  get '/recipes' do
+  	@recipes = Recipe.all
+  	erb :index
+  	# binding.pry
+  end
+
+  get '/recipes/:id' do
+  end
+
+  get '/recipes/:id/edit' do
   end
 end
