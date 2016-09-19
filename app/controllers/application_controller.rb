6ce65fdf563b382575877713d351545859bcf2d3
auranbuckles <auran59@gmail.com>
2016-09-19 19:25:44 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -11,15 +12,43 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/recipes' do
+  get '/recipes' do #show index page
   	@recipes = Recipe.all
   	erb :index
-  	# binding.pry
   end
 
-  get '/recipes/:id' do
+  get '/recipes/new' do # form to create new recipe
+  	erb :new
+ 	end
+
+  get '/recipes/:id' do # patch and delete requests
+  	@recipe = Recipe.find_by_id(params[:id])
+    erb :show
   end
 
-  get '/recipes/:id/edit' do
+  get '/recipes/:id/edit' do # form to edit
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
   end
+
+  patch '/recipes/:id' do # update a recipe
+  	@recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do # create a new recipe
+  	@recipe = Recipe.create(params)
+  	redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do # delete recipe
+	  @recipe = Recipe.find_by_id(params[:id])
+	  @recipe.delete
+	  redirect to '/recipes'
+	end
+
 end
