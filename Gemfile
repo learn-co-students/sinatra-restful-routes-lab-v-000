29ad4d052600d65c751909ef043b6f19c3838e4e
Scott Bewick <bewick.scott@gmail.com>
2016-12-22 11:49:29 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'sqlite3'
 gem 'tux'
 gem 'rake'
 gem 'require_all'
+gem 'rack', '~> 1.6.4'
 
 group :test do
   gem 'rspec'
---
+++ db/migrate/20161221192330_change_recipes.rb
@@ -0,0 +1,7 @@
+class ChangeRecipes < ActiveRecord::Migration
+  def change
+    change_table :recipes do |t|
+      t.change :cook_time, :string 
+    end  
+  end
+end
