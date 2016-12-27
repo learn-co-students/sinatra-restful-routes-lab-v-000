18a9e15a92f623ee8673b94b7b52c66bb0cd0992
github username <github email address>
2016-12-27 13:23:09 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
 
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
-    @recipe.destroy
+    @recipe.delete
     redirect to '/recipes'
   end
 
