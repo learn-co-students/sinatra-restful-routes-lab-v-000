22e8435371960ac833568d4c6c365594ee1c89fc
bentonwong <bentonwong@gmail.com>
2017-03-18 22:44:55 -0700

    add recipes table

---
+++ db/migrate/20170319053957_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration
+  def up
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
+  end
+
+  def down
+  	drop_table :recipes
+  end
+
+end
