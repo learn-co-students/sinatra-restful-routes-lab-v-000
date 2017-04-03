4d3fc465dca26c3e7cf187d1af7ecd03bb551b7d
walwoodr <walwoodr@gmail.com>
2017-04-03 11:30:51 -0500

    Add outline of controllers and corresponding view files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,40 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    # create instance of recipe and save, then redirect to display
+  end
+
+  get '/recipes/:id' do
+    # get recipe from db
+    # including delete button
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    # including delete button?
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    # modify instance of recipe and save then redirect to display
+  end
+
+  delete '/recipes/:id' do
+    # delete the instance of the recipe
+  end
+
 end
