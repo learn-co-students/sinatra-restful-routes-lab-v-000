3a3f0766b32872b552bc12d937686d0459cfa1cc
seth <sethalexander@protonmail.com>
2017-04-09 14:11:02 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    "Hello World"
+    # erb :index
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
 end
