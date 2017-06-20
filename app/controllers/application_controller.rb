6f7791604c27a9a54972a037b0f5b18e0cb7e354
Will-Sauer <will@heueso.com>
2017-06-20 12:41:12 -0700

    finished:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,59 @@
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-  get '/' do
-    erb :index
-  end
+configure do
+  set :public_folder, 'public'
+  set :views, 'app/views'
 end
+
+
+    get '/' do
+    erb :index
+    end
+
+    get '/recipes' do
+      erb :index
+    end
+    
+
+    get '/recipes/new' do
+    erb :new
+    end
+    
+    
+    post '/recipes' do
+         Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+         redirect "/recipes/#{Recipe.last.id}"
+    end
+    
+    get '/recipes/:id' do
+        @id = params[:id]
+        erb :show
+    end
+    
+    get '/recipes/:id/edit' do
+          @id = params[:id]
+          erb :edit
+    end
+
+    patch '/recipes/:id' do
+      @id = params[:id]
+      Recipe.find(@id).update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+      redirect "/recipes/#{@id}"
+    end
+    
+
+    patch '/recipes/:id' do
+      @id = params[:id]
+      Recipe.find(@id).update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+      redirect "/recipes/#{@id}"
+    end
+
+
+    delete '/recipes/:id' do
+          
+            Recipe.delete(params[:id])
+            erb :index
+    end
+
+
+
+end#eoc
