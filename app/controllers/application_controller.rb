393b91c5b1eb5a15ca98279580b539748fd087cc
Samson Yuwono <samson.yuwono@gmail.com>
2017-06-28 17:47:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,34 +14,36 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/recipes" do
-    @recipes = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    redirect to "/recipes/#{@recipes.id}"
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
   end
 
 
   get "/recipes/:id" do
-    @recipes = Recipe.find_by_id(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
   get "/recipes/:id/edit" do  #load edit form
-    @recipes = Recipe.find_by_id(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
 
   patch "/recipes/:id" do #edit action
-    @recipes = Recipe.find_by_id(params[:id])
-    @recipes.name = params[:name]
-    @recipes.ingredients = params[:ingredients]
-    @recipes.cook_time = params[:cook_time]
-    @recipes.save
-    redirect to "/recipes/#{@recipes.id}"
+    raise @recipe.inspect
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete "/recipes/:id/delete" do #delete action
-    @recipes = Recipe.find_by_id(params[:id])
-    @recipes.clear
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
     redirect to "/recipes"
+
   end
 
 end
