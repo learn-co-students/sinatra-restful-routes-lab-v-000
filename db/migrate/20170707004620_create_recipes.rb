e0cd8d445d8f71205aeb5f47cc9e5f45d91519fa
James Fernandez <jffernan@comcast.net>
2017-07-07 01:26:06 +0000

    Done.

---
+++ db/migrate/20170707004620_create_recipes.rb
@@ -0,0 +1,10 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :Recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+
+    end
+  end
+end
