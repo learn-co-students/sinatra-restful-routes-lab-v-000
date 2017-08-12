fd4e88f8fadcb39fdba12353a07ac7b4eb7f74ba
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-12 22:02:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,43 +4,50 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
 
-  get '/recipes/new' do
+  get '/recipes/new' do #This allows input for new recipe
     erb :new
   end
 
-  get '/recipes' do
+  get '/recipes' do # This displays all the recipes
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by(params[:id])
+  get '/recipes/:id' do # This shows one specific recipe
+    @recipe = Recipe.find(params[:id])
     erb :show
   end
 
-  get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by(params[:id])
+  get '/recipes/:id/edit' do # This shows a recipe to be edited
+    @recipe = Recipe.find(params[:id])
     erb :edit
   end
 
-  patch '/recipes/:id' do
+  patch '/recipes/:id' do # This makes those edits
     # This is where we will make our edits
-    @recipe = Recipe.find_by(params[:id])
+    @recipe = Recipe.find(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  post '/recipes' do
+  post '/recipes' do # This makes a recipe
     #this is where I'll make the new recipe
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-
     redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
   end
 
 end
