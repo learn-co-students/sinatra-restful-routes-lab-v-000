3782c7f36b0f8ad33b4fe3c988801f5f51473274
Annette Maza <nettymaza@gmail.com>
2017-10-12 18:51:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,55 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    redirect to '/recipes'
+  end
+
+  #Index action and allows the view to access all the posts in the database
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  #Get request that loads new.rb form to create a new recipe.
+  get '/recipes/new' do
+    erb :new
+  end
+
+  #Create action, responds to post request and creates a new recipe based on params set on form new.rb and saves it to database.
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  #Will load edit.erb page
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do #edit action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do #delete action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+  end
+
 end
