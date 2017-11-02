75050d66f631517c1246fd9adcf8e90450d00b49
Rony Boeck <ramg.9@live.com>
2017-11-02 20:34:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,53 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-    erb :index
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
+
+  get '/recipes/new' do
+    erb :'/recipes/new'
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params['name'], ingredients: params['ingredients'], cook_time: params['cook_time'])
+    redirect to "/recipes/#{@recipe.id}/"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'/recipes/show'
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    erb :'index'
+  end
+
+  get '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    erb :'index'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'recipes/edit'
+  end
+
+  patch '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:recipe_name]
+    @recipe.ingredients = params[:recipe_ingredients]
+    @recipe.cook_time = params[:recipe_cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+
   end
 end
