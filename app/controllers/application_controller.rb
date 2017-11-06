1151b6787544794b03d46caec97c9fcac3ca3206
Denis Bondin <dsbondin@gmail.com>
2017-11-06 02:04:05 +0000

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/recipes' do
     @recipes = Recipe.all
     erb :index
+  end
+
+  get '/recipes/:id' do
+    
   end
 
 
