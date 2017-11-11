2c601025540e9a87aa4230ba61950625f83815fa
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-11-11 14:25:09 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -27,22 +26,27 @@ class ApplicationController < Sinatra::Base
 	post '/recipes' do 
 		@recipe = Recipe.create(params)
 		@recipe.save
-		redirect '/recipes/:id'
+		redirect 'recipes/' + "#{@recipe.id}" 
 	end
 
 
 	get '/recipes/:id/edit' do
-		@recipe = Recipe.find(params[:id])
+		@recipe2 = Recipe.find(params[:id])
 		erb :edit  	
 	end
 
-	patch '/recipes/:id/edit' do
+	patch '/recipes/:id' do
 		@recipe = Recipe.find(params[:id])
 		recipe.name = params[:name]
 		recipe.ingredients = params[:ingredients]
 		recipe.cook_time = params[:cook_time]
 		recipe.save
-		redirect '/recipes/:id'
+	end
+
+  delete '/recipes/:id/delete' do 
+		recipe = Recipe.find(params[:id])
+    recipe.delete
+		erb :index
 	end
 
 end
