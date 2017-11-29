16a421ab90aa9e635a169864aa2b3f8ee7f707cc
Adam Fudge <coolfudge@gmail.com>
2017-11-29 01:58:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,34 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/recipes' do
+    erb :index
+  end
 
+  # get '/recipes/:id' do
+  #   @recipe = Recipe.find_by_id(params[:id])
+  #   erb :show
+  # end
+  #
+  # get '/recipes/:id/edit' do
+  #   erb :edit
+  # end
+  #
+  # patch '/recipes/:id' do
+  #   @recipe = Recipe.find_by_id(params[:id])
+  #   @recipe.name = params[:name]
+  #   @recipe.ingredients = params[:ingredients]
+  #   @recipe.cook_time = params[:cook_time]
+  #   @recipe.save
+  #   redirect to "/recipes/#{@recipe.id}"
+  # end
+  #
+  # get '/recipes/new' do
+  #   erb :new
+  # end
+  #
+  # post '/recipes' do
+  #   @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  #   redirect to "/recipes/#{@recipe.id}"
+  # end
 end
