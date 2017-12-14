980ccf6392fe88d820d894e825b7d96981f8d626
Mel Rudick <melissarudick@MacBook-Air.local>
2017-12-13 20:04:31 -0600

    five more errors to go

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,5 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-
+require_relative '../app/controllers/application_controller.rb'
 require_all 'app'
