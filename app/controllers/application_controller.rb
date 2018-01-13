2a49bd187036f0b4dc43a2000f36326d0f61f764
Hannah Carnes <hecarnes@gmail.com>
2018-01-13 18:53:45 -0500

    got index working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
 end
