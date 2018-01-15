8c56e713e484607bf81fd5c3de75074304eaed75
Susan Fowler <infopro94@gmail.com>
2018-01-15 02:35:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,20 @@ require 'pry'
 class ApplicationController < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-  
+
 
   get '/recipes/new' do
     erb :new
   end
 
-  get '/recipes' do #display list contain links to each show page errors
+  get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
+    # binding.pry
     erb :show
   end
 
@@ -25,18 +26,18 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  post '/recipes' do #create, save & and redirect errors
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
   patch '/recipes/:id' do
-    @recipe = Recipe.find_byid(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect to '/recipes/#{@recipe.id}'
-  end
-
-  post '/recipes' do #create, save & and redirect errors
-    @recipe = Recipe.create(params)
-    redirect to '/recipes/#{@recipe.id}'
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
