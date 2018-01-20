f5ce12be902f1d726f3ccbd1187e4c16b4072983
Sherry El Garhy <sherryptk@gmail.com>
2018-01-20 21:36:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -7,9 +8,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/recipes'do
+  get '/recipes' do
     @recipes = Recipe.all
 
     erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
   end
 end
