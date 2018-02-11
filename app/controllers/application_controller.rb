e337d510e15a18d8df8e7d4e88b7d6e618f999b2
github username <github email address>
2018-02-11 14:10:53 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,49 +1,47 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  set :views, Proc.new { File.join(root, "../views/") }
 
   get '/recipes/new' do
-  	erb :new
+    erb :new
   end
 
-	get '/recipes' do
-  	@recipes = Recipe.all
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
 
   get '/recipes/:id' do
-  	@recipe = Recipe.find_by_id(params[:id])
-  	erb :show
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
   end
 
   get '/recipes/:id/edit' do
-  	@recipe = Recipe.find_by_id(params[:id])
-  	erb :edit
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
   end
 
   patch '/recipes/:id' do
-  	@recipe = Recipe.find_by_id(params[:id])
-  	@recipe.name = params[:name]
-  	@recipe.ingredients = params[:ingredients]
-  	@recipe.cook_time = params[:cook_time]
-  	@recipe.save
-
-  	redirect to "/recipes/#{@recipe.id}"
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
-	post '/recipes' do
-  	@recipe = Recipe.create(params)
-
-  	redirect to "/recipes/#{@recipe.id}"
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
-  	@recipe = Recipe.find_by_id(params[:id])
-  	@recipe.delete
-  	redirect to '/recipes'
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+  redirect to '/recipes'
   end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211181834) do
+ActiveRecord::Schema.define(version: 20180211185949) do
 
 end
