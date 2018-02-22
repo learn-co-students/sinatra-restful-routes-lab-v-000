8bd5e14dbe55eedf87f407df1c1ccc7fecc1f3a7
Meredith Reece <mardebugger@gmail.com>
2018-02-22 23:03:09 +0000

    in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,38 @@
 class ApplicationController < Sinatra::Base
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    # erb :index
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
+  end
+
+  post '/recipes' do
+    # create a new Recipe, save to database
+    # redirect to show page /recipes/Recipe.id
+  end
+
+  get '/recipes/new' do
+    erb :new_recipe
+    # need to make this file, see stub of form in index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show_recipe
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect '/recipes'
   end
 end
