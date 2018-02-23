6ffdf8b67fcb83d27f6d9b9b045c1fec2bec3edc
Sally Zasloff <szasloff@gmail.com>
2018-02-23 00:11:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,12 +30,13 @@ class ApplicationController < Sinatra::Base
 
 
   patch 'recipes/:id' do
+    binding.pry
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cooking_time = params[:cooking_time]
     @recipe.save
-    binding.pry
+
     redirect "/recipes/#{@recipe.id}"
   end
 
