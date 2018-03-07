bc82c5d1ea63f44e794d0d6d5975544d6b1b2677
Miwa <paintfx@gmail.com>
2018-03-07 03:10:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-    erb :index
-  end
+
+  # CREATE =========================
 
   get '/recipes/new' do
     erb :new
@@ -16,22 +16,48 @@ class ApplicationController < Sinatra::Base
     redirect "recipes/#{recipe.id}"
   end
 
+  # READ =========================
+
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
   get '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     erb :show
   end
+
+  # UPDATE =========================
+
+  # post 'recipes/:id/edit' do
+  #   @recipe = Recipe.find(params[:id])
+  #   redirect "recipes/#{recipe.id}/edit"
+  # end
 
   get 'recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
     erb :edit
   end
 
-  patch 'recipes/id' do
+  patch 'recipes/:id' do
     @recipe = Recipe.find(params[:id])
     @recipe.name = params[:name]
-    @recipe.ingredient = params[:ingredient]
+    @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
     erb :show
+  end
+
+  # DELETE =========================
+
+  delete 'recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect '/'
   end
 end
