37d5192db0512943de9acb7acf691857d34a9402
Blakeley Bond <burrick2003@yahoo.com>
2018-03-11 01:37:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
 
   end
-  delete '/recipes/:id/delete' do
+  post '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
     redirect to '/recipes'
