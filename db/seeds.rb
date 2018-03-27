8926ac90a5ac9b0b7c7bfc82ffdd83158d4262bd
Sharmina Akhtar <sakht318@gmail.com>
2018-03-27 09:21:38 +0000

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+pizza = Recipe.create(name: "Pizza", ingredients: "dough, sauce, cheese", cook_time: "40 minutes")
+sandwich= Recipe.create(name: "Sandwich", ingredients: "peanut-butter, jelly, bread", cook_time: "0 minutes")
