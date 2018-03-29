8f0f5c83097ee613488d0a47f18ce7859f05303a
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-29 15:58:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,58 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get 'recipes/new' do
+    #3 #displays a new recipe form(render)
+    erb :new
+  end
+
+  post '/recipes' do
+    #3#create and save this new recipe to db
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/:#{@recipe.id}"
+    #reroute to recipes/:id?
+    erb :index
+  end
+
+  get 'recipes/:id' do
+    #4 display a single recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    #add a form that allows a user to delete a single recipe
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    #5 renders a form to edit a single recipe
+  end
+
+  patch '/recipes/:id' do
+    #5 edits an existing recipe based on url
+    #reroutes to the show page (recipes/:id)
+  end
+
+  get '/recipes' do
+    #6 displays all the recipes in the database (an index action)
+    binding.pry
+    @recipes = Recipe.all
+
+    erb :index
+  end
+
+  delete '/recipes/:id/delete' do
+    #7 delete the entry and redirect to the index page
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
+
+
+
 end
