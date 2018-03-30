e114d93a362aefacedb831a92cb4605b1120a777
Sean Oughton <seanoughton@gmail.com>
2018-03-30 18:09:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,11 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  post '/recipes' do
+    @recipe = Recipe.create(:name=>params[:name], :ingredients=>params[:ingredients], :cook_time=>params[:cook_time])
+    redirect to '/recipes'
+  end
+
 
 
 
