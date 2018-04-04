a1924127f4aaa68d99dda35db0e411869117feea
Eden Mesfun <emesfun@gmail.com>
2018-04-04 21:25:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,15 @@ class ApplicationController < Sinatra::Base
     erb :'index'
   end
 
+  get '/recipes/new' do
+    erb :'new'
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time] )
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :'recipes'
@@ -23,4 +32,5 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find_by_id(params[:id])
     erb :'edit'
   end
+
 end
