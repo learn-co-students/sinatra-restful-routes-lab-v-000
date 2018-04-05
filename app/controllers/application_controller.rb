317c0964d078ad00c0042d71df05c96552d81e83
Eden Mesfun <emesfun@gmail.com>
2018-04-05 13:42:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,9 +28,22 @@ class ApplicationController < Sinatra::Base
     erb :'specific_recipe'
   end
 
+  get '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
+    erb :'deleted'
+  end
+
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :'edit'
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(params[:name])
+    @recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+
+    redirect to ("/posts/#{@post.id}")
   end
 
 end
