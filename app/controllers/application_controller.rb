4501f86c8866a57cee2eaed36464b216af2696c9
Araba Taylor <araba.taylor20@gmail.com>
2018-04-07 20:44:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,5 +36,11 @@ class ApplicationController < Sinatra::Base
     erb :recipes
   end
 
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+  end
+
+
 
 end
