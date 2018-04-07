2bfe06270baf4f4c0d85d5a6a09b454295c0d3f1
Araba Taylor <araba.taylor20@gmail.com>
2018-04-07 21:02:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,21 @@ class ApplicationController < Sinatra::Base
     erb :recipes
   end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe=Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect '/recipes/#{@recipe.id}'
+  end
+
+
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
