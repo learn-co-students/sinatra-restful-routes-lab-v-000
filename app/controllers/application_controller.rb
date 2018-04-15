1ce684182692269f5250e973d3b300ee88ae40b0
Chaim Reisman <crreisman@gmail.com>
2018-04-15 18:12:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-  get '/' do
-    erb :index
-  end
-end
--- db/migrate/20180415163959_create_recipes.rb
+++ db/migrate/20180415163959_create_recipes.rb
@@ -1,4 +1,4 @@
-class CreateRecipes < ActiveRecord::Migration[4.2]
+class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :name
