412ef9b249c4d9df77c796db75d1c3e1d327a303
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-04-24 03:18:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,64 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
-    erb :index
+    redirect to '/recipes'
   end
+
+  get '/recipes' do
+
+    @recipes = Recipe.all
+    erb :index
+
+  end
+
+  get '/recipes/:id' do
+    @recipe9 = Recipe.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  delete '/recipes/:id/delete' do #delete action
+    @recipe3 = Recipe.find_by(params[:id])
+    @recipe3.delete
+    redirect to '/recipes'
+  end
+
+  get '/recipes/:id/edit' do  #load edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+patch '/recipes/:id' do #edit action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.name = params[:name]
+  @recipe.ingredients = params[:ingredients]
+  @recipe.cook_time = params[:cook_time]
+  @recipe.save
+  redirect to "/recipes/#{@recipe.id}"
 end
+
+
+
+  get '/create' do
+    erb :create
+  end
+
+  post '/recipes' do
+    puts params
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    recipe.save
+    redirect to '/recipes'
+  end
+
+
+end
+
+# In the application_controller.rb, set up a controller action
+# that will render a form to create a new recipe.
+# This controller action should create and save this new recipe to the database.
