c96e67919d68d60e0624bc182f6470a9eefc5143
ecssiah <ecssiah@gmail.com>
2018-05-02 20:03:17 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,60 @@
 class ApplicationController < Sinatra::Base
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes' do
+    @recipes = Recipe.all
+
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    recipe = Recipe.create(
+      name: params[:name], ingredients: params[:ingredients], 
+      cook_time: params[:cook_time])
+
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    recipe.name = params[:name] if params[:name] != ""
+    recipe.ingredients = params[:ingredients] if params[:ingredients] != ""
+    recipe.cook_time = params[:cook_time] if params[:cook_time] != ""
+    recipe.save
+
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+
+    redirect to '/recipes'
+  end
+
+  get '/RESET' do
+    Recipe.destroy_all
+    Recipe.connection.execute("DELETE from sqlite_sequence where name = 'recipes'")
+  end
+  
 end
