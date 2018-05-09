07a85b07d3b8fc8360595e6c7c60aa77e3e54f80
Frank McCallion <fkmccallion@gmail.com>
2018-05-09 02:48:41 +0000

    Done.

---
+++ db/migrate/20180508122719_create_recipes_table.rb
@@ -0,0 +1,9 @@
+class CreateRecipesTable < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.text :name
+      t.text :ingredients
+      t.text :cook_time
+    end
+  end
+end
