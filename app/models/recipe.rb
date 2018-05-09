fd0f10c8c94b2975f7ad86f0bfce3997f3fc463f
suzy baldwin <gbaldwin24@comcast.net>
2018-05-09 17:24:06 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,11 @@
 #Placeholder for a model
 class Recipe < ActiveRecord::Base
+  def changes
+    create_table :recipes do |t|
+    t.string :name
+    t.string :ingredients
+    t.string :cook_time
+  end
 
 
 end
