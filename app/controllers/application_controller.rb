2ad07187aa1ed97f82bd9c238f99276da7b0a1bf
suzy baldwin <gbaldwin24@comcast.net>
2018-05-09 17:18:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,33 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+
+      erb :show
+
+  end
+  delete '/recipes/:id/delete' do
+
+  @recipe = Recipe.delete(params[:id])
+
+  erb :show
+
+end
+get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+end
+get '/recipes/new' do
+
+    erb :new
+  end
+
+
 end
