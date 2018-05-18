45ec56c71983fc483d429735ab8a6683a4b2a15e
Raiet Intraub <raietsaeed@gmail.com>
2018-05-18 01:32:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,30 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
-  get '/' do
+
+  # Index Page
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  # # Create Page
+  # get '/recipes/new' do
+  #   erb :new
+  # end
+  #
+  # # Create Object Action
+  # post '/recipes' do
+  #   recipe = Recipe.create(params)
+  #   redirect to "/recipes/#{recipe.id}"
+  # end
+  #
+  # # Show Page
+  # get '/recipes/:id' do
+  #   @recipe = Recipe.find(params[:id])
+  #   erb :show
+  # end
+
 
 
 
