36a7d7e91e7698cc9f073973b67c869938dd89c5
Zac Willmington <zac.willmington@gmail.com>
2018-05-28 17:04:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,22 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+      @recipes =  Recipe.all
+      erb :index
+  end
+
+  get '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      erb :show
+  end
+
+  
+
 end
