31c4df372f8a94edac03abc6474e885dfdbbafd0
Victoria Meng <nights1ststar@protonmail.com>
2018-05-31 03:51:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,47 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    redirect "/recipes"
+  end
+
+  get '/recipes' do
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    erb :show
+  end
+
+  delete '/recipes/:id/delete' do
+    current_recipe.delete
+    redirect "/recipes"
+  end
+
+  get '/recipes/:id/edit' do
+    erb :edit
+  end
+
+  patch '/recipes/:id/edit' do
+    binding.pry
+    current_recipe.update(params)
+    redirect "/recipes/:id"
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  helpers do
+    def current_recipe
+      Recipe.find(params[:id])
+    end
+  end
+
 end
