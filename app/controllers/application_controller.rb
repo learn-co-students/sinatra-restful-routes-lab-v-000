78d5fa932ab191b764897645c4418f60064c6850
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-31 17:17:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,20 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find(params[:id])
     erb :recipe
   end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  post '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
 end
