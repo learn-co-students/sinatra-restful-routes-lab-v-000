987688c038492f30a58837463337170d30d279b9
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-31 18:56:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
-    erb :index
+    "Welcome!"
+
   end
 
   get '/recipes' do
@@ -12,28 +14,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :recipe
   end
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
 
-  post '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
   end
 
-  get '/recipes/new' do
+  get '/new' do
     erb :new
   end
 
-  post'/recipes' do
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    redirect '/recipes/#{@recipe.id}'
+  post '/new' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
   end
 end
