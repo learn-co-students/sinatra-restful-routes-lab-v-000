af53f9259436f52ae265f7109de7c910a2e9bd49
Petya Djanovska <petya.djanovska@gmail.com>
2018-06-01 22:39:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
   get '/' do
+    erb :index
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
 end
