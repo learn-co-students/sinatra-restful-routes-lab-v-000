b42022d00ab857dbc03f2bfc985e664bb7e37720
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-03 16:49:31 -0400

    Rspeclemented NEW action & SHOW action (except for the delete button). Added layout template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,36 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get '/' do
     erb :index
+  end
+
+  # NEW action
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    recipe = Recipe.new
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  # SHOW action
+  get '/recipes/:id' do
+    # Recipe.find throws an exception on failure,
+    # whereas find_by just returns nil
+    recipe = Recipe.find_by(id: params[:id])
+
+    if recipe
+      "#{recipe.name + "<br>" + recipe.ingredients + "<br>" + recipe.cook_time}"
+    else
+      "RECIPE NOT FOUND"
+    end
   end
 end
