dddfda0ad575b6350e5b5f3613412529fbaade8d
Vanessa Toro <vanetoro84@gmail.com>
2018-06-05 02:09:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,36 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+
     erb :index
   end
+
+  get '/recipes' do
+    # binding.pry
+    @recipes = Recipe.all
+    binding.pry
+    erb :index
+  end
+
+  get '/recipes/new' do
+    # binding.pry
+
+
+    erb :new
+  end
+
+  post 'recipes/post' do
+    @recipe = Recipe.new(params)
+    @recipe.save
+    redirect '/recipes'
+  end
+
+
+  # get "/recipes/#{recipe.id}" do
+  #
+  #   erb :"recipe/show"
+  # end
+
 end
