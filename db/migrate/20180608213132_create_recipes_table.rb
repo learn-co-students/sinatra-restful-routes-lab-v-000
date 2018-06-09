2dcaa794216112a226503b2d20e3f2d2ac190717
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-06-09 14:44:09 -0400

    Done.

---
+++ db/migrate/20180608213132_create_recipes_table.rb
@@ -0,0 +1,13 @@
+class CreateRecipesTable < ActiveRecord::Migration
+  def up
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.integer :cook_time
+    end
+  end
+
+  def down
+    drop_table :recipes
+  end
+end
