43719a7901ed646c36450d5ac144eaf16563056e
netzay <netzelally@gmail.com>
2018-06-17 10:24:41 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,48 +2,60 @@ require "./config/environment"
 require "pry"
 
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-  get '/' do
+	configure do
+		set :public_folder, 'public'
+		set :views, 'app/views'
+	end
+	get '/' do
+	end
 
-  end
+	get '/recipes' do
+		@recipe = Recipe.all
+		erb :index
+	end
 
-  get '/recipes' do
-  	#binding.pry
-  	@recipes = Recipe.all
-    erb :index
-  end
+	post '/recipes' do
+		@recipe = Recipe.new(:id => params[:id], :name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+		@recipe.save
+		redirect "/recipes/#{@recipe.id}"
+	end
 
-  post '/recipes' do
-      @recipes = Recipe.new(:id => params[:id], :name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-      @recipes.save
+	get '/recipes/new' do
+		erb :new
+	end
 
-      redirect "/recipes/#{@recipes.id}"
-  end
+	get '/recipes/:id' do
+		@recipe = Recipe.find_by_id(params[:id])
+		erb :show
+	end
+	
+	get '/recipes/:id/edit' do
+		@recipe = Recipe.find_by_id(params[:id])
+		erb :edit 
+	end
+	
+	patch '/recipes/:id' do #edit action
+		@recipe = Recipe.find_by_id(params[:id])
+		@recipe.name = params[:name]
+		@recipe.ingredients = params[:ingredients]
+		@recipe.cook_time = params[:cook_time]
+		@recipe.save
+
+		redirect "/recipes/#{@recipes.id}"
+	end
 
 
-  get '/recipes/new' do
-  	erb :new
-  end
+
+	delete '/recipes/:id/delete' do
+		@recipe = Recipe.find_by_id(params[:id])
+		@recipe.delete
+		erb :show
+	end
 
 
-  get '/recipes/:id' do
-  	@recipes = Recipe.find_by(:id => params[:id])
-  	erb :show
-  end
+	
 
-  get '/recipes/:id/edit' do
-  	@recipes = Recipe.find_by(:id => params[:id])
-  	erb :edit
 
-  end
-  post '/show' do
-  	 @recipes = Recipe.find_by(:id => params[:id])
 
-  	redirect "/recipes/#{@recipes.id}"	
-  end
 
-  
 end
