2651052c81a7525be512035800c9da36d874f160
Lucy Wall <lhwall@gmail.com>
2018-06-17 03:18:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,24 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+ get "/recipes" do
+   erb :index
+ end
+
+ get "recipes/new" do
+   erb :new
+ end
+
+ post "/recipes" do
+   @recipe = Recipe.create(:name => params[:name], ingredients => params[:ingredients], cook_time => params[:cook_time])
+   redirect to "/recipes/#{@recipe.id}"
+ end
+
+ get "/recipes/:id" do
+   @recipe = Recipe.find(params[:id])
+   #binding.pry
+   erb :show
+ end
+
 end
