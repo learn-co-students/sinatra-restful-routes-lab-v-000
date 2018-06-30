8883346328aa4dcab12f063ec77630385742713a
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-06-29 18:12:07 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,31 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do #delete action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+
+
+  # helpers do
+  #   def all
+  #
+  #     @recipes.each do |recipe|
+  #       "#{recipe.name} for more infomration /recipes/#{recipe.id}"
+  #     end
+  #   end
+  # end
 end
