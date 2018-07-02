493db2a1e9989bedcbedf45fa269a8b45afe1caa
taryn yaeger <tsyaeger@gmail.com>
2018-07-02 19:47:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,26 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/' do
     erb :index
   end
+
+  get '/recipes' do 
+    erb :recipes 
+  end
+  
+  get '/recipes/new' do
+    erb :new 
+  end
+  
+  post '/recipes' do
+    Recipe.create(params)
+    redirect '/recipes/#{recipe.id}'
+  end
+  
+  get '/recipes/:id' do
+    erb :show
+  end
+
 end
