471606da5c7508d221d0bd1289e20ff6fbc10423
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-19 17:20:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,21 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
-  get '/' do
+
+  get '/recipes' do
     erb :index
   end
   
+  get '/recipes/:id' do
+    # find by id
+    
+    erb :show
+  end
   
+  get '/recipes/:id/edit' do 
+    # find by id
+    
+    erb :edit
+  end
   
 end
