db9dbe03ee6ecb24bf7aaf63e472b6d6c0821190
Elaine Anderson <eparie@bu.edu>
2018-07-19 01:07:14 +0000

    saftey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,35 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipe = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+  erb :new
+end
+
+get "/recipes/:id" do
+  @recipe = Recipe.find_by_id(params[:id])
+erb :show
+end
+
+
+get '/recipes/:id/edit' do  #load edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do #edit action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+
 end
