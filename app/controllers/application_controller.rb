6933bd3898cd9a2ae10d6d89378a145c860f6b50
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-19 06:52:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < Sinatra::Base
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/' do
     erb :index
   end
+  
+  
+  
 end
