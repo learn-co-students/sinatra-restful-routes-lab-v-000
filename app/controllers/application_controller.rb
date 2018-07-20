d54f7e0dd8264ad11306901241dcfa3831b6507d
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-20 23:04:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     @recipe = Recipe.create(params)
     
-    redirect '/recipes/#{Recipe.last.id}'
+    redirect '/recipes/#{@recipe.id}'
   end
   
   get '/recipes/:id' do
@@ -35,19 +35,23 @@ class ApplicationController < Sinatra::Base
   end
   
   patch '/recipes/:id' do 
-    # find and update recipe 
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     
     redirect '/recipes/#{@recipe.id}'
   end
 
+=begin
   put '/recipes/:id' do
     # find and replace recipe, may not need this one
     
     redirect '/recipes/#{@recipe.id}'
   end
-  
+=end 
+
   delete '/recipes/:id/delete' do
-    # delete recipe
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.delete
     
     redirect '/recipes'
   end
