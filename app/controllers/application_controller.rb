ef635341e2ca38c3cdc0f02479cc935b1ce50370
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-23 20:04:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
-class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+require './config/environment'
 
-  get '/' do
-    @recipes = Recipe.all
-    erb :index
+class ApplicationController < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
+  # get '/' do
+  #   @recipes = Recipe.all
+  #   erb :index
+  # end
+  get '/recipes/new' do
+    erb :new
   end
 
   get '/recipes' do
@@ -14,8 +15,23 @@ class ApplicationController < Sinatra::Base
   erb :index
 end
 
-  get '/recipes/new' do
-    erb :new
+get '/recipes/:id' do
+@recipe = Recipe.find_by_id(params[:id])
+erb :show
+end
+
+get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   post '/recipes' do
@@ -23,24 +39,6 @@ end
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  get '/recipes/:id' do
-  @recipe = Recipe.find_by_id(params[:id])
-  erb :show
-end
-
-get '/recipes/:id/edit' do  #load edit form
-    @recipe = Recipe.find_by_id(params[:id])
-    erb :edit
-  end
-
-patch '/recipes/:id' do
-  @recipe = Recipe.find_by_id(params[:id])
-  @recipe.name = params[:name]
-  @recipe.ingredients = params[:ingredients]
-  @recipe.cook_time = params[:cook_time]
-  @recipe.save
-  redirect to "/recipes/#{@recipe.id}"
-end
 
 delete '/recipes/:id/delete' do
   @recipe = Recipe.find_by_id(params[:id])
