b52ab4ecd1ef9e611abe535150181e09c40af0e6
Rebecca Tong <rtong@buffalo.edu>
2018-07-24 17:51:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
+    binding.pry
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe.save
 
   end
 
