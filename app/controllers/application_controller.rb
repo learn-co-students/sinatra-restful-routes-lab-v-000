fb31959cd0ce3b2754987a4fb310f13f0699a496
Judy McGoldrick <jjeanius@verizon.net>
2018-07-25 04:06:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    recipes = Recipes.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    erb :index
+  end
+
+
 end
