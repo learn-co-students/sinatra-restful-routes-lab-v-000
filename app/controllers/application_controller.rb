fd418c05ce6e0e2fb8cb1050ebb689b47116375b
Brett Weisberg <weisberg.brett@gmail.com>
2018-07-26 00:46:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,23 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
+    end
+
+  post '/recipes' do
+    recipe = Recipe.new(:name => params[:name],:ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    if recipe.save
+      erb :recipes
+    else
+      redirect '/'
+    end
   end
+
+    get '/recipes' do
+      @recipes =  Recipe.all
+      erb :recipes
+  end
+
 end
