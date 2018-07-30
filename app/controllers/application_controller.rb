007b9ec483e194b891977ec84568bcfdf6bda5fa
Christine Cua <cloudycua@gmail.com>
2018-07-30 15:06:09 +0000

    add 'use Rack::MethodOverride' to config.ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,46 +4,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/recipes/new' do
-    erb :new
+  get "/" do
+    redirect to "/recipes"
   end
 
-  post '/recipes' do
-    @recipe = Recipe.create(params)
-    redirect '/recipes/#{@recipe.id}'
-  end
-
-  get '/' do
-    erb :index
-  end
-
-  get '/recipes' do
+  get "/recipes" do #loads index page
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/:id' do
+  get "/recipes/new" do #loads new form
+    erb :new
+  end
+
+  post "/recipes" do  #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get "/recipes/:id" do  #loads show page
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
-  delete '/recipes/:id/delete' do
-    @recipe = Recipe.find_by_id(params[:id])
-    @recipe.delete
-    redirect '/recipes'
-  end
-
-  get '/recipes/:id/edit' do
+  get "/recipes/:id/edit" do #loads edit form
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
 
-  patch '/recipes/:id' do
+  patch "/recipes/:id" do  #updates a recipe
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
-    erb :show
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete "/recipes/:id/delete" do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
   end
 
 end
