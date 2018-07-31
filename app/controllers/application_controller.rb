db2f7126097c18abf691a044a80d17bd9fd40423
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-31 06:40:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,31 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     @recipe.save
-    
+    redirect "/recipes/#{Recipe.last.id}"
   end
+  
+  get '/recipes/:id' do
+    @current_recipe = Recipe.find_by_id(params[:id])
+    erb :show 
+  end
+  
+  get '/recipes/:id/edit' do
+    @current_recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end 
+  
+  patch '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+    
+  delete '/recipes/:id' do
+    Recipe.find_by_id(params[:id]).destroy
+    redirect '/recipes'
+  end
+  
 end
