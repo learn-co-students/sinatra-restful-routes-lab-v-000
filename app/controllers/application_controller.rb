92a1d8f8e758cd6317386869171738852ef9f9e3
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-31 05:37:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  
+  get '/recipes' do
     erb :index
+  end
+  
+  get '/recipes/new' do
+    erb :new 
+  end
+  
+  post '/recipes' do
+    @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.save
+    
   end
 end
