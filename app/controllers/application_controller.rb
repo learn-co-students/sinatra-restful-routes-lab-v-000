f01ae3153b0127b185153740c9fc01d9126175c3
Christopher P. Dundon <chris.dundon@gmail.com>
2018-08-03 16:50:05 -0400

    lab complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,44 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+	get '/recipes' do
+		@recipes = Recipe.all
+		erb :recipes
+	end
+
+	get '/recipes/new' do
+		erb :new
+	end
+
+	get '/recipes/:id' do
+		@recipe = Recipe.find_by(id: params[:id].to_i)
+		erb :show
+	end
+
+	delete '/recipes/:id/delete' do
+		@recipe = Recipe.find_by(id: params[:id].to_i)
+		@recipe.delete
+		erb :index
+	end
+
+	get '/recipes/:id/edit' do
+		@recipe = Recipe.find_by(id: params[:id].to_i)
+		erb :edit	
+	end
+
+	patch '/recipes/:id' do #edit action
+		@recipe = Recipe.find_by_id(params[:id])
+		@recipe.name = params[:name]
+		@recipe.ingredients = params[:ingredients]
+		@recipe.cook_time = params[:cook_time]
+		@recipe.save
+		
+		redirect to "/recipes/#{@recipe.id}"
+	end
+	 
+	post '/recipes' do
+		@recipe = Recipe.create(name: params[:name], ingredients:  params[:ingredients], cook_time: params[:cook_time])
+		redirect to "/recipes/#{@recipe.id}"
+	end
 end
--- config.ru
+++ config.ru
@@ -1,7 +1,10 @@
 require './config/environment'
 
+=begin
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
+=end
 
+use Rack::MethodOverride
 run ApplicationController
