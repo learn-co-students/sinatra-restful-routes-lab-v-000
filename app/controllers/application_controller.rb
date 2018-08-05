c0889cb74fd05334fd8a92dc3d93fce96ce255b7
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-05 02:09:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    
+  end
+
 end
