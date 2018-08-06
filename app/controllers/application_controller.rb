3f6c60135c446b6e98b41a7ec6cd5ccc018e6efe
Anu Khambete <anukhambete@gmail.com>
2018-08-06 20:02:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,32 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    "Hello World"
+  end
+
+  get '/recipes' do
+    #binding.pry
+    @recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
 
   get '/recipes/new' do
     erb :newrecipe
   end
 
-  post '/recipes/new' do
-    binding.pry
-    redirect "/recipes/#{Recipe.last.id}"
-    erb :newrecipe
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe.save
+    #binding.pry
+    redirect "/recipes/#{@recipe.id}"
   end
 
 
