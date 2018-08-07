a1bcb2ab19b32c2e1f7a2f1222b6442e3869ee0d
treverandersen <trever.r.andersen@gmail.com>
2018-08-07 11:44:47 -0500

    setup controller action to show index page and allow access to all the recipes, setup index view to display recipes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
 end
