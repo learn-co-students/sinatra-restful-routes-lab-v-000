cdef33138133886dbcab38133be2e82a5b72c156
treverandersen <trever.r.andersen@gmail.com>
2018-08-07 12:54:24 -0500

    changed the controller action for delete to redirect to index, added a line to config to allow PATCH, PUT, and DELETE requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   delete '/recipes/:id/delete' do 
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
-    redirect to '/recipes'
+    redirect to '/'
   end
   
 end
