2e1ed0fc4dbdb7fbf20f3c5c7f9b1eb3e612129d
treverandersen <trever.r.andersen@gmail.com>
2018-08-07 11:51:02 -0500

    setup controller action to get the recipe id and render the show view, setup the show view to display the recipe name, ingredients, and cook time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < Sinatra::Base
     @recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'/show'
+  end
+
+  
 end
