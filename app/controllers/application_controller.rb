a04a5e6850a6374530c95651e3d52fdd1d67c400
pracniscate <lorena.giddens@outlook.com>
2018-08-15 21:02:26 -0400

    code is clean, tests need to be looked at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect "/recipes/#{@recipe.id}"
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   # delete action
