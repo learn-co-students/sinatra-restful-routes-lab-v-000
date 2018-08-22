c03c8a4bf664967a56c1fea14f7720a6145f1eaa
Brent Bauman <brentbauman86@gmail.com>
2018-08-22 00:51:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,19 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
   get '/recipes' do
     @recipes = Recipe.all
-# binding.pry
     erb :index
-  end
-
-  post '/recipes' do
-    @recipes = Recipe.create(params)
-    redirect to '/recipes/#{@recipe.id}'
   end
 
   get '/recipes/new' do
     erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
@@ -25,23 +25,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by_id(params[:id])
-    # binding.pry
+    @recipe = Recipe.find(params[:id])
     erb :edit
   end
 
   patch '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
+    @recipe = Recipe.find(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect to '/recipes/#{@recipe.id}'
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
     recipe = Recipe.find(params[:id])
     recipe.delete
-    redirect to '/recipes'
+
+    redirect to "/recipes"
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,4 @@
 #Placeholder for a model
 class Recipe < ActiveRecord::Base
-  # 
-  # attr_accessor :name, :ingredients, :cook_time
-  #
-  # @@all = []
-  #
-  # def initialize(params)
-  #   @name = params[:name]
-  #   @ingredients = params[:ingredients]
-  #   @cook_time = params[:cook_time]
-  #   @@all << self
-  # end
-  #
-  # def self.all
-  #   @@all
-  # end
+
 end
