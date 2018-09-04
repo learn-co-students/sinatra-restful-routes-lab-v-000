2df9df8ab0fbe677c167f98267bbf50463c1f919
Wasif Khan <waasif.khaan@gmail.com>
2018-09-04 17:04:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,48 +1,52 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
+  # register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-    
-    erb :index
+
+  get '/recipes/new' do #loads new form
+    erb :new
   end
-  
-  get '/recipes' do 
+
+  get '/recipes' do #loads index page
     @recipes = Recipe.all
     erb :index
-  end 
-  get 'recipes/new' do 
-    erb:new
-  end 
-  post '/recipes' do 
-    @recipe= Recipe.create(:name => params[:name], :ingredients => params[:ingredients],:cook_time => params[:cook_time])
-    redirect to "/recipes/#{@recipe.id}"  
-  end 
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
-  erb :show
   end
-  
-  get '/recipes/:id/edit' do  #load edit form
+
+  get '/recipes/:id' do  #loads show page
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do #loads edit form
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
-  
-  patch '/articles/:id' do #edit action
+
+  patch '/recipes/:id' do  #updates a recipe
     @recipe = Recipe.find_by_id(params[:id])
-    @recipe.title = params[:title]
-    @recipe.content = params[:content]
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
     @recipe.save
     redirect to "/recipes/#{@recipe.id}"
-end
+  end
 
-delete '/articles/:id/delete' do #delete action
+  post '/recipes' do  #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do #delete action
   @recipe = Recipe.find_by_id(params[:id])
   @recipe.delete
   redirect to '/recipes'
 end
-  
-  
+
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,3 @@
-#Placeholder for a model
-class Recipe < ActiveRecord::Base 
-end 
+class Recipe < ActiveRecord::Base
+
+end
