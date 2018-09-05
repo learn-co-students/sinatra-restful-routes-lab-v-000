0e8953854898ba98c408ac0bf3c38f3f1952d1b4
Jennifer Stoos <jstoos@bitlaw.com>
2018-09-05 16:08:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,15 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get 'recipe/new' do
+  get '/recipes/new' do
     erb :new
   end
 
-  post '/recipe' do
+  post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    redirect to  "/recipes/#{recipe.id}"
+    # raise @recipe.inspect
+    #erb :show  #--does not "redirect" as test requires
+    redirect to "/recipes/#{@recipe.id}" #does not carry @recipe forward
   end
 
   get '/recipes/:id' do
@@ -23,11 +25,24 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name= params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
     redirect to '/recipes'
   end
-
 
 end
