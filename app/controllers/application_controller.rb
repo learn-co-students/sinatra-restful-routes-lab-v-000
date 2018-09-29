a021e430a5f2e7a145318638ab97795cf0d69007
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-29 15:41:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
+
+  
 end
--- db/migrate/20180929152803_recipes.rb
+++ db/migrate/20180929152803_recipes.rb
@@ -1,6 +1,6 @@
 class Recipes < ActiveRecord::Migration
   def change
-    create_table recipes do |t|
+    create_table :recipes do |t|
       t.string :name
       t.string :ingredients
       t.string :cook_time
