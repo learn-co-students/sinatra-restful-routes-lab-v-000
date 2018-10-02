345f2e8a7395b522009cb7e80e89292fe9c373ae
Tao Liu <taoliudesign@gmail.com>
2018-10-02 22:31:27 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,24 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/' do
+    @recipes = Recipe.all
     erb :index
   end
+  
+  get '/recipes' do
+    #show all recipes
+    @recipes = Recipe.all
+    erb :recipes
+  end
+  
+  get '/recipes/new' do
+    
+  end
+  
+  post '/recipes' do
+
+  end
+  
 end
