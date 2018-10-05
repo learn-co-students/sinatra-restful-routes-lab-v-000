52fcb3573b4e87a35a06e57b2eca5000ea38cbc9
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-05 23:07:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ end
     erb :edit
   end
   
-  patch '/recipes/:id' do  #updates a recipe
+  patch '/recipes/:id' do 
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
@@ -38,12 +38,12 @@ end
     redirect to "/recipes/#{@recipe.id}"
   end
   
-   post '/recipes' do  #creates a recipe
+   post '/recipes' do
     @recipe = Recipe.create(params)
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  delete '/recipes/:id/delete' do #delete action
+  delete '/recipes/:id/delete' do 
   @recipe = Recipe.find_by_id(params[:id])
   @recipe.delete
   redirect to '/recipes'
