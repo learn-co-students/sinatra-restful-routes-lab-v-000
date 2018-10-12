9e8eaa8ef1241fe5f5025b7d2b838df6252307fe
AlwinaO <aoyewole2586@gmail.com>
2018-10-11 23:18:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,23 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+
     erb :index
   end
+
+  get '/recipes/:id' do
+    # binding.pry
+    @recipe = Recipe.find_by_id(params[:id])
+
+    erb :show
+  end
+
+
 end
