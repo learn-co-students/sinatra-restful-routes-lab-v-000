c46eeb77cb153ed757a7a1477b36368e8831f46e
Nathan  Visak <nathan.visak@gmail.com>
2018-10-30 17:30:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
