a2c762225d5e243ecc9926377434552fa78b3031
Lane Miller <chris.lane.miller@gmail.com>
2018-10-30 18:07:45 -0400

    removed pre-written action, get "/" do, from application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-    erb :index
-  end
+
+  # code actions here!
+
 end
