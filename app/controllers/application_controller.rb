0138b0ca308eee6a6b5e98fb347c0442067628de
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-08 23:55:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     
+    redirect to '/recipes/#{@recipe.id}'
   end
 
   get '/recipes' do
@@ -16,6 +23,11 @@ class ApplicationController < Sinatra::Base
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe =Recipe.find_by_id(params[:id])
+    erb :edit
   end
 
 end
