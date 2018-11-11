e088d387e0c287f22aabcc89ad90d123704bf1c4
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-11 20:57:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get "/recipes" do
+    erb :index
+  end
 
+  get "/recipes/new" do
+    erb :new
+  end
+
+  post "/recipes" do
+    Recipe.create(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients])
+    redirect to "/recipes/#{last_created_recipe.id}"
+  end
+
+  get "/recipes/:id" do
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    current_recipe.update(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients])
+    redirect to "/recipes/#{current_recipe.id}"
+  end
+
+  delete "/recipes/:id" do
+    current_recipe.delete
+  end
+
+  helpers do
+    def all_recipes
+      Recipe.all
+    end
+
+    def current_recipe
+      Recipe.find_by_id(params[:id])
+    end
+
+    def last_created_recipe
+      Recipe.last
+    end
+  end
 end
