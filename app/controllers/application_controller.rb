aaf13af1fac9cf3c01b9b16b1cdbdea90af5214a
Justin Kenney <justinkenney13@gmail.com>
2018-11-14 16:58:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,26 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    # binding.pry
+    erb :index
+  end
+
+  post '/recipes' do
+    # binding.pry
+    # raise params.inspect
+    @recipe = Recipe.create(params)
+    redirect to ("recipes/#{@recipe.id}")
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
 
 end
