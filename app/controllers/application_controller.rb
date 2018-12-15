b703edc1a628331430c8afafbada1b6a038e9d7b
Wendy Calderon <wendycalderon@gmail.com>
2018-12-15 04:24:38 +0000

    Create actions+view to render form+create+save a recipe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
 
-  # code actions here!
-
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
 end
