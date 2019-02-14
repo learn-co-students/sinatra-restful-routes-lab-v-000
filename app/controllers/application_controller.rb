438a235e2f8bc69cb840f5fbc83ef51d19dd71db
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-14 06:01:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,24 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes' do
-      @recipes = Recipe.all
+     @recipes = Recipe.all
       erb :index
   end
 
   get '/recipes/new' do
       erb :new
   end
+
+  post '/recipes' do
+      @recipe = Recipe.create(params)
+      redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+      @recipe = Recipe.find_by_id(params[:id])
+      erb :show
+  end
+
+
 
 end
