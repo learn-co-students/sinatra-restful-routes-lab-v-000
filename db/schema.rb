881cb25d685c8b36b92f5fff5a6427a7e17cf590
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-14 02:14:04 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190214014250) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "ingredients"
-    t.string "cook_time"
-  end
+ActiveRecord::Schema.define(version: 20190214021335) do
 
 end
