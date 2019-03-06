2283255720af31fa8c3ea30d046d549fa74a5f04
Ashley Wong <ashwong04@gmail.com>
2019-03-06 16:02:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,22 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
   
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/recipies/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/articles/#{@article.id}"
+  end
+  
   post '/recipes/:id/delete' do
-    
+    Recipe.delete_at(params[:id])
   end
   
   
