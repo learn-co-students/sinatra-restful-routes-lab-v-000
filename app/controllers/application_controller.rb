030735b2faa506aa6f060e4392549dab607f0455
Samantha brake <sammy.brake@gmail.com>
2019-03-08 17:23:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  # code actions here!
+  
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :recipes
+  end 
+  
+  get '/recipes/new' do
+  erb :new
+  end
+  
+  get '/recipes/:id' do
+  @arecipe = Recipe.find_by_id(params[:id])
+  erb :show
+end
+  
+  post '/recipes' do
+  @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  @recipe.save
+  redirect to "/recipe/#{@recipe.id}"
+  end
 
 end
