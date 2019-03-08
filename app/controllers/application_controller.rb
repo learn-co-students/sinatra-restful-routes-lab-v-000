5aa0216c238a1f95e08914a41275b3cb9a7910b9
Samantha brake <sammy.brake@gmail.com>
2019-03-08 18:01:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,26 +4,39 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
   
-  get '/recipes' do 
-    @recipes = Recipe.all
-    erb :recipes
-  end 
-  
+  ## route to submit a new recipe
   get '/recipes/new' do
   erb :new
   end
   
+  ##new recipe form posts to this route, creates a new recipe, and redirects to the show page
   post '/recipes' do
   @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
   @recipe.save
   redirect "/recipe/#{@recipe.id}"
   end
   
+  #show the information for one recipe based on the id
   get '/recipes/:id' do
   @recipe = Recipe.find_by_id(params[:id])
   erb :show
   end
   
+  #display all the recipes 
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :recipes
+  end 
+  
+  ## Below are the controller routes to update a recipes
+  
+  ##Controller route to delete 
+  
+  delete '/recipes/:id/delete' do #delete action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  redirect to '/recipes'
+end
   
 
 end
