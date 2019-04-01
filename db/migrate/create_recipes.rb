4c262dc65b726ba6ce778d7dd3d093c85752b570
Cheryl K <cherylkelson@icloud.com>
2019-04-01 14:49:06 +0000

    Done.

--- db/migrate/create_recipes.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecipes < ActiveRecord::Migration
-  def change
-    create_table :recipes do |t|
-      t.string :name
-      t.string :ingredients
-      t.string :cook_time
-    end
-  end
-end
