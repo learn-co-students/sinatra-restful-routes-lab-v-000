5db614d15a90036e68237624a67653256c8b0f16
Cheryl K <cherylkelson@icloud.com>
2019-04-01 11:53:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect to 'recipes/#"[@recipe.id}"
+    redirect to "/recipes/#{@recipe.id}"
   end
 
 
