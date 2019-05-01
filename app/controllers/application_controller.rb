52eaaab11df83ae9ebd3d5bf9f3b572fe2b21db7
Thomas Gray <tgray017@gmail.com>
2019-05-01 12:31:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < Sinatra::Base
   get '/recipes' do
     erb :index
   end
+  
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params["name"], :ingredients => params["ingredients"], :cook_time => params["cook_time"])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    
+  end
 end
