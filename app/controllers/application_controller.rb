00dabb8a19d04bc7810873a3227c1459b5e34554
Laura Messa Travin <laura.travin@gmail.com>
2019-05-10 03:30:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,57 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  #index show articles
   get '/recipes' do
     @recipes = Recipe.all
-
     erb :index
   end
-
-
-
-  get '/recipes/:id' do
-
-  end
-
+  #show form to create new article
   get '/recipes/new' do
-    
+    erb  :new
   end
+
+
+
+
+
+  #new post created
+  post '/recipes' do
+    @new_recipe= Recipe.create(:name => params[name], :ingredients => params[ingredients], :cook_time => params[cook_time])
+    redirect to '/recipes/'+ @new_recipe.id
+#      redirect "/recipes/#{@new_recipe.id}"
+  end
+
+
+
+  #show post
+  get '/recipes/:id' do
+    @recipe= Recipe.find_by(:id =>  params[:id])
+    erb :show
+  end
+
+
+  #edit post
+  get '/recipes/:id/edit' do
+
+    @recipe= Recipe.find_by(:id =>  params[:id])
+    erb :edit
+  end
+
+    # end
+
+  # delete '/recipes/:id/delete' do
+  #
+  #   @recipe = Recipe.destroy(params[:id])
+  #   redirect '/recipes'
+  # end
+
+  delete '/recipes/:id' do
+
+           @del= Recipe.find(params[:id])
+
+           @del.destroy
+           erb :delete
+     end
 
 end
