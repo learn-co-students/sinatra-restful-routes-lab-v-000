8909dc6e87131af90c15b8e87c4c6cfa307f1c69
Paul Fell <swellpf@gmail.com>
2019-06-19 20:29:19 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect "/recipes/#{recipe.id}"
+    redirect "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id' do
