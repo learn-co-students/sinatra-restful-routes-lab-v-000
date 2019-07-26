3919d20c723bfe769cb78f2858e98a8593379436
Mat Anderson <matandersonlv@hotmail.com>
2019-07-26 19:50:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,29 +10,34 @@ class ApplicationController < Sinatra::Base
   
   # New action -
   get '/recipes/new' do
+    
     erb :new
   end
   
   post '/recipes' do
     @recipe = Recipe.create(params)
+    
     redirect to "/recipes/#{@recipe.id}"
   end
   
   # Show action -
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
+    
     erb :show
   end
   
   # Index action - 
   get '/recipes' do
   @recipes = Recipe.all
+  
   erb :index
   end
   
   # Edit action -
   get '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
+    
     erb :edit
   end
   
@@ -49,6 +54,7 @@ class ApplicationController < Sinatra::Base
   delete '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     @recipe.delete
+    
     redirect to '/recipes'
   end
   
