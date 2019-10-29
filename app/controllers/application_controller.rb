bc8ba1e1c707ee4a4851b49a021d5988a1f78d09
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-29 22:22:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
  
-  put '/recipes/:id' do #edit action
+  patch '/recipes/:id' do #edit action
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
