ac02bcdd57f70b0a2306c47a55003eb94ab3eb32
Olivia Kesler <oakesler@protonmail.ch>
2020-01-07 19:08:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,13 +28,20 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
  
-patch '/articles/:id' do #edit action
-  @article = Article.find_by_id(params[:id])
-  @article.title = params[:title]
-  @article.content = params[:content]
-  @article.save
-  redirect to "/articles/#{@article.id}"
-end
+  patch '/recipes/:id' do #edit action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  delete '/recipes/:id' do #delete action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 
 
   # code actions here!
