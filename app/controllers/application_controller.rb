5358a98b23dc984ca966f107cdbe9c03dcd59510
Jonathan Fernandez <jfern20@gmail.com>
2020-02-26 01:09:25 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,5 +34,16 @@ class ApplicationController < Sinatra::Base
     erb :edit 
   end
   
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time= params[:cook_time]
+    @recipe.save 
+    redirect to "/recipes/#{@recipe.id}"
+
+  end 
+  
+  
 
 end
