dbd2dcb0d14538a19cccb9aafb9bafc82c0681c1
github username <github email address>
2020-05-14 19:19:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-    get '/recipes'
+    get '/recipes' do
       @recipes = Recipe.all
       erb :index
     end
 
-    get '/recipes/new'
+    get '/recipes/new' do
       erb :new
     end
 
     post '/recipes' do
-      @recipe = Recipe.create(name:params[:name], ingredients:params[:ingredients], cook_time:params[:cook_times])
+      @recipe = Recipe.create(name:params[:name], ingredients:params[:ingredients], cook_time:params[:cook_time])
       redirect to "/recipes/#{ @recipes.id }"
     end
 
