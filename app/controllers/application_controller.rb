9335a6e470d70678c2e0a4796e8a5196d0e40667
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-22 22:16:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    erb :show
+    redirect to "/recipes/#{recipe.id}"
   end
 end
 
